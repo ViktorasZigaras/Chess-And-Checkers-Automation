a0a8c97b931ff10929357ea1e91842d9908a73e8
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-03-29 21:04:56 +0300

    transformation

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
 
 const rowCount = 8
 const colCount = 8
+const rowHeight = 75
+const colWidth = 75
+const board = document.querySelector( '.board' ) //All
 
 const drawBoard = () => {
     // const board = document.getElementsByClassName( 'board' )
-    const board = document.querySelector( '.board' ) //All
+    
     let content = ''
     // const rowHeight = 100 / rowCount
     // const colWidth = 100 / colCount
-    const rowHeight = 75
-    const colWidth = 75
     let color = ''
     for ( let y = 0; y < rowCount; y++ ) {
         content += `<div class="row" style="height: ${ rowHeight }px">`
@@ -29,22 +30,23 @@ const drawBoard = () => {
     }
     board.innerHTML = content
 }
-// var object = document.createElement('container');
-// object.style.width= "500px";
-// object.style.height= "600px";
 
 drawBoard()
 
 const setUpPieces = () => {
     let piece = {}
     piece.type = 'pawn'
-    piece.x = 5
-    piece.y = 3
+    piece.x = 0
+    piece.y = 0
     pieces.push(piece)
 
-    const board = document.querySelector( '.board' )
-    let content = `<div class="piece" id="0" style="top: ${ 75 * piece.y }px; left: ${ 75 * piece.x }px"></div>`
-    board.innerHTML += content
+    let piecePhysical = document.createElement( 'div' )
+    piecePhysical.id = 0
+    piecePhysical.style.top = rowHeight * piece.y + 'px'
+    piecePhysical.style.left = colWidth * piece.x + 'px'
+    piecePhysical.className = 'piece'
+    piecePhysical.innerHTML = 'Pawn 1'
+    board.appendChild( piecePhysical )
 }
 
 const pieces = []
@@ -53,25 +55,36 @@ setUpPieces()
 const startTimer = () => {
     const piecePhysical = document.querySelector( '.piece' )
     const piece = pieces[ piecePhysical.id ]
-    setInterval(
-        () => {
-            let x = piece.x + getRngInteger( -1, 1 )
-            if ( x < 0 ) x = 0
-            else if ( x >= colCount ) x = colCount - 1
-            piece.x = x
-            piecePhysical.style.left = x * 75 + 'px'
-            let y = piece.y + getRngInteger( -1, 1 )
-            if ( y < 0 ) y = 0
-            else if ( y >= rowCount ) y = rowCount - 1
-            piece.y = y
-            piecePhysical.style.top = y * 75 + 'px'
-            console.log( x, y )
-            
-        }, 100
-    )
+    timer = setInterval( () => { downMovement( piece, piecePhysical ) }, 300 )
+    // setInterval( () => { randomMovement( piece ) }, 300 )
 }
 
+let timer
 startTimer()
+
+const downMovement = ( piece, piecePhysical ) => {
+    piece.y++
+    piecePhysical.style.top = piece.y * rowHeight + 'px'
+    console.log( piece.y )
+    if ( piece.y === rowCount - 1 ) {
+        piecePhysical.innerHTML = 'Queen'
+        clearInterval( timer )
+    }
+}
+
+const randomMovement = ( piece, piecePhysical ) => {
+    let x = piece.x + getRngInteger( -1, 1 )
+    if ( x < 0 ) x = 0
+    else if ( x >= colCount ) x = colCount - 1
+    piece.x = x
+    piecePhysical.style.left = x * colWidth + 'px'
+    let y = piece.y + getRngInteger( -1, 1 )
+    if ( y < 0 ) y = 0
+    else if ( y >= rowCount ) y = rowCount - 1
+    piece.y = y
+    piecePhysical.style.top = y * rowHeight + 'px'
+    console.log( x, y )
+}
 
 const getRngInteger = ( min, max ) => {
     return Math.floor( Math.random() * ( max - min + 1 ) ) + min
